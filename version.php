abcf8e8966c90ee749b48e4e92770e0b535b6792
Andreas Koch <51958234+abkoch@users.noreply.github.com>
2023-07-17 14:00:04 +0200

    Update version.php

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_imprint';
 $plugin->release = '1.0.1';
-$plugin->version = 2023071700;
+$plugin->version = 2023071701;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
